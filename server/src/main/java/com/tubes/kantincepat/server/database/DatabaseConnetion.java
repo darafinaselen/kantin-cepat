fbf1236cd7751ba96970dc2a8b3aa20d5d755659
F1D022101 <4yusltr@gmail.com>
2025-12-03 00:18:49 +0800

    fix pelanggan (sudah bisa semua)

--- server/src/main/java/com/tubes/kantincepat/server/database/DatabaseConnetion.java
+++ server/src/main/java/com/tubes/kantincepat/server/database/DatabaseConnetion.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnetion {
     private static final String URL = "jdbc:postgresql://localhost:5432/kantin_cepat_db";
     private static final String USER = "postgres"; 
-    private static final String PASSWORD = "darakunci25"; 
+    private static final String PASSWORD = "pw"; 
     
     public static Connection getConnection() throws SQLException {
         try {
